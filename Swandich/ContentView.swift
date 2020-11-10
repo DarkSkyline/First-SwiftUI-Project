4baca92d377c2f0da86bf3d534d9de57420b3db7
Filipe Ferreira <filipesferreira94@gmail.com>
2020-11-10 17:12:39 +0000

    Initial Commit

---
+++ Swandich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swandich
+//
+//  Created by Filipe Ferreira on 10/11/2020.
+//  Copyright © 2020 Filipe Ferreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
